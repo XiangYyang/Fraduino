72d3063c5152ac6ca6f83ff6245c4cc7bd8e15df
向阳 <hinata.hoshino@qq.com>
2023-06-26 14:17:00 +0800

    add: makefile;

---
+++ mcu/inc/ma_main.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0
+/**
+ * @file    ma_main.h
+ * @author  向阳 (hinata.hoshino@foxmail.com)
+ * @brief   主程序
+ * @version 1.0
+ * @date    2023-06-26
+ *
+ * @copyright Copyright (c) 向阳, all rights reserved.
+ *
+ */
+#if !defined(_INCLUDE_MA_MAIN_H_)
+#define _INCLUDE_MA_MAIN_H_ 1
+
+#endif // _INCLUDE_MA_MAIN_H_
---
+++ mcu/src/ma_main.c
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0
+/**
+ * @file    ma_main.c
+ * @author  向阳 (hinata.hoshino@foxmail.com)
+ * @brief   主程序
+ * @version 1.0
+ * @date    2023-06-26
+ *
+ * @copyright Copyright (c) 向阳, all rights reserved.
+ *
+ */
+#include "ma_main.h"
+#include "gpio.h"
+#include "main.h"
+
+extern void SystemClock_Config(void);
+extern void PeriphCommonClock_Config(void);
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+    PeriphCommonClock_Config();
+    MX_GPIO_Init();
+    while (1) {
+        // nop
+    }
+}
